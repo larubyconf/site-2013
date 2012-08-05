c565eb349ed32878cd01dd4fdd1415edb5e00ce6
Coby Randquist <randquistcp@gmail.com>
2012-08-05 06:33:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Larubyconf2013::Application.config.secret_token = 'afd323598affd10f00bef4b35f32242959c89cf9f65d830d1dac5be46dbd88d646391b4f2aad4c2bd1adf4678dbd091cd9247cfee426d4be518cc63b880f90e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Larubyconf2013::Application.config.session_store :cookie_store, key: '_larubyconf2013_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Larubyconf2013::Application.config.session_store :active_record_store
