0bd75818b0249c43aca9a8e8a7a1db03212809eb
Coby Randquist <randquistcp@gmail.com>
2012-08-20 00:26:09 -0700

    deploy fixes

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,5 @@
-set :application, "site-stage"
-
 role :web, "alpha.larubyconf.com"
 role :app, "alpha.larubyconf.com"
-role :db,  "alpha.larubyconf.com"
+role :db,  "alpha.larubyconf.com", :primary => true
 
 set :rails_env, "staging"
