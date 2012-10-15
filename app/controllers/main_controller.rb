f099981d3c170a527a168c166344516a84b048bb
Coby Randquist <randquistcp@gmail.com>
2012-10-15 12:19:29 -0700

    news_item addition

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,7 @@ class MainController < ApplicationController
   def index
     @sponsors = Sponsor.order('level asc, sponsored_at asc')
     @photos = Photo.order("sort_index, created_at asc")
+    @news_items = NewsItem.display
   end
 
   def admin
--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,3 +1,7 @@
 class NewsItem < ActiveRecord::Base
   attr_accessible :body, :display_date, :expire_date, :headline, :user_id
+
+  belongs_to :user
+  
+  scope :display, where("display_date < '#{Time.now}'").order('display_date desc')
 end
