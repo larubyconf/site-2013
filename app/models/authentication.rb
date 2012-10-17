54366893512c23883d3f8e2fb5178e7903a0967a
Coby Randquist <randquistcp@gmail.com>
2012-10-17 16:50:33 -0700

    added provider name method to fix issue with Open ID display

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -2,4 +2,12 @@ class Authentication < ActiveRecord::Base
   attr_accessible :provider, :uid, :user_id
 
   belongs_to :user
+
+  def provider_name
+    if provider == "openid"
+      "Open ID"
+    else
+      provider.titleize
+    end
+  end
 end
