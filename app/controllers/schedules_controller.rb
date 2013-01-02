341c7100ba2793d7e3cd3c34ce6d167917720d50
Coby Randquist <randquistcp@gmail.com>
2013-01-01 18:35:21 -0800

    Change schedule order

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,6 +2,6 @@ class SchedulesController < ApplicationController
   def show
     @workshops_thursday = Workshop.day("2013-02-21")
     @workshops_friday = Workshop.day("2013-02-22")
-    @presentations = Proposal.accepted
+    @presentations = Proposal.accepted.order("presented_at")
   end
 end
